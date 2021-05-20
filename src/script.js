1fccd8f424390bd6676d39a28108cd6d03bb2185
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-20 04:27:03 +0300

    initial page + form HTML only

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
-let createButton = document.getElementById("create")
-createButton.addEventListener("click", (e) => {
-    
+let createButton = document.getElementById("createBtnM")
+let walletName = document.getElementById("walletName")
+let balance = document.getElementById("balance")
+let desc = document.getElementById("desc")
+createButton.addEventListener('click', (e)=>{
+    e.preventDefault()
+    console.log(desc.value);
 })
+
